e8d337234b05289adac6415eed46280a0363b852
zhongjianxin <jxzhong@thoughtworks.com>
2022-01-07 15:35:46 +0800

    [Jason] update the api return

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World";
+		return "Hello k8s new World";
 	}
 
 	public static void main(String[] args) {
