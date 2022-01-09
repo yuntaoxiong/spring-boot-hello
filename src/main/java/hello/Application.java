7f1115ea29faa23b273f26394512eda6df93f1e2
zhongjianxin <jxzhong@thoughtworks.com>
2022-01-09 23:40:41 +0800

    [Jason] update the api reposen body

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello k8s new World !!!";
+		return "Hello k8s new World !!! go go go";
 	}
 
 	public static void main(String[] args) {
