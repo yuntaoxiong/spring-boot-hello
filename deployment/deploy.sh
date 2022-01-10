027e72c1e8a13aa4a90cfe45306546ad07b41f33
zjx-immersion <jianxin_home@163.com>
2022-01-10 21:44:45 +0800

    [Jason] change the jenkins file and deploy script 2

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -3,5 +3,5 @@
 VERSION=$SERVICE-$BUILD_NUMBER
 IMAGE="$SERVICE:latest"
 
-sed "s#{{service}}#$SERVICE#g; s#{{image}}#$IMAGE#g; s#{{namespace}}#$NAMESPACE#g; s#{{version}}#$VERSION#g;" ./deployment/hello-app.yaml | kubectl apply -f ./deployment/
+sed "s#{{service}}#$SERVICE#g; s#{{image}}#$IMAGE#g; s#{{namespace}}#$NAMESPACE#g; s#{{version}}#$VERSION#g" ./deployment/hello-app.yaml | kubectl apply -f ./deployment/
 
