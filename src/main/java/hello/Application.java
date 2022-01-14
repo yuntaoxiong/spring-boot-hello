c322f2d45a463b56c6b18a422151b546b0daa040
zjx-immersion <jianxin_home@163.com>
2022-01-14 16:58:47 +0800

    [Jason] add k8s

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,20 +2,41 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-	@RequestMapping("/")
-	public String home() {
-		return "Hello k8s new digital world!";
-	}
+    private String myName = "";
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    @RequestMapping("/")
+    public String home() {
+        return "Hello k8s new digital world!";
+    }
+
+    @RequestMapping("/test")
+    public String test() {
+        return "test new API";
+    }
+
+    @RequestMapping("/name/{name}")
+    public String getName(@PathVariable("name") String name) {
+        return this.myName + " - your name" + name;
+    }
+
+    @PostMapping("/name")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String createName(@RequestBody String name) {
+        String mName = "my name: " + name;
+        this.myName = mName;
+        return mName;
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
